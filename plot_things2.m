6147942bfc1b8402b91888ba3cf3a21823ed9bb7
Angelia Wang <angeliawang1@gmail.com>
2021-01-10 17:34:00 -0600

    fixed a minus sign
    because I'm dumb and can't multiply

--- plot_things2.m
+++ plot_things2.m
@@ -7,7 +7,7 @@ set(0,'DefaultAxesFontSize',15,'defaultaxeslinewidth',2,...
 % weights
 weights = 0:1:10;
 omega = 0:0.1:20;
-expos = -1:0.05:3;
+expos = -3:0.05:3;
 Ts = 10.^expos; %0.1:0.5:1000;
 covar_T = NaN(length(weights), length(Ts));
 
@@ -43,7 +43,7 @@ for w_i = 1:num_vars
     w = indep_var(w_i);
     
     syms M(x) %indep variable is omega
-    M(x) = (-w*xi_1_0*(1-x.^2*tau+w)-w*xi_2_0*(1-x.^2*tau+w)-w.^2*zeta_0*(1+x.^2))/...
+    M(x) = (-w*xi_1_0*(1-x.^2*tau+w)-w*xi_2_0*(1-x.^2*tau+w)+w.^2*zeta_0*(1+x.^2))/...
         ((1+x.^2)*((1+x.^2)*(1+x.^2*tau^2)+4*w*(1-x.^2*tau)+4*w.^2));
     
     if normalize_xcorr
@@ -73,7 +73,7 @@ for w_i = 1:num_vars
         % with respect to omega
         % where
         integrand = @(x, zeta, t, xi1, xi2, we, bin) ...
-            2*((-we.*xi1.*(2-2*x.^2*t+2*we)-we.*xi2.*(2-2.*x.^2.*t+2.*we)-2.*we.^2.*zeta.*(1+x.^2))./...
+            2*((-we.*xi1.*(2-2*x.^2*t+2*we)-we.*xi2.*(2-2.*x.^2.*t+2.*we)+2.*we.^2.*zeta.*(1+x.^2))./...
         ((1+x.^2).*((1+x.^2).*(1+x.^2.*t^2)+4.*we.*(1-x.^2.*t)+4.*we.^2))).*(1-cos(x.*bin))./x.^2;
         covar_T(w_i, t_i) = integral(@(x) integrand(x,zeta_0,tau,xi_1_0,xi_2_0,w,T), -Inf, Inf)/(T^2);
         
@@ -106,26 +106,27 @@ hold off
 subplot(2, 1, 1)
 set(gca, 'yscale', 'linear')
 xlabel('\omega')
-ylabel('$M_1^* M_2 + M_2 M_1^*$', 'interpreter', 'latex')
+ylabel('$\frac{1}{2} (M_1^* M_2 + M_2 M_1^*)$', 'interpreter', 'latex')
 legend([w_1, w_end], ['w=', num2str(weights(1))], ...
     ['w=', num2str(weights(end))], 'location', 'northeast')
 subplot(2, 1, 2)
 set(gca, 'yscale', 'linear')
 xlabel('T', 'interpreter', 'latex')
-ylabel('var ( $\frac{1}{T} \int_0^T M_1(t_1) dt_1 \frac{1}{T} \int_0^T M_2(t_2) dt_2 $ )', 'interpreter', 'latex')
+ylabel('$Cov(M_1, M_2)$', 'interpreter', 'latex')
+%ylabel('var ( $\frac{1}{T} \int_0^T M_1(t_1) dt_1 \frac{1}{T} \int_0^T M_2(t_2) dt_2 $ )', 'interpreter', 'latex')
 legend([w2_1, w2_end], ['w=', num2str(weights(1))], ...
-    ['w=', num2str(weights(end))], 'location', 'southwest')
+    ['w=', num2str(weights(end))], 'location', 'southeast')
 % subplot(3, 1, 3)
 % xlabel('T', 'interpreter', 'latex')
 % ylabel('Without normalization by T^2', 'interpreter', 'latex')
 % legend([w3_1, w3_end], 'w=0', 'w=100', 'location', 'southwest')
-hgsave(fack, ['psd_var', suffix,'.fig'])
-saveas(fack, ['psd_var', suffix,'.png'])
+hgsave(fack, ['variance_studies/psd_var', suffix,'.fig'])
+saveas(fack, ['variance_studies/psd_var', suffix,'.png'])
 
 % now we save some shite:
 if normalize_xcorr
-    save(['var_psd_data', suffix,'.mat'], 'var_T', 'weights', ...
+    save(['variance_studies/var_psd_data', suffix,'.mat'], 'var_T', 'weights', ...
     'Ts', 'omega', 'M_numint', 'numerical_psds')
 else
-    save(['var_psd_data', suffix,'.mat'], 'covar_T', 'weights', 'Ts', 'omega')
+    save(['variance_studies/var_psd_data', suffix,'.mat'], 'covar_T', 'weights', 'Ts', 'omega')
 end
